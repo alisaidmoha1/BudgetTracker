4fbb158f91734a8426f07ad3dbac81f5c45e65b3
alisaidmoha1 <alisaidmoha1@gmail.com>
2021-10-04 14:44:24 -0400

    Added user login

---
+++ Budget.Data/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget.Data
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+        
+    }
+}
---
+++ Budget.Data/Expense.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget.Data
+{
+    public class Expense
+    {
+        [Key]
+        public int ExpenseId { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        [Required]
+        public decimal Amount { get; set; }
+        [Required]
+        [Display(Name ="Repeat")]
+        public bool IsRepeat { get; set; }
+        public string Note { get; set; }
+    }
+}

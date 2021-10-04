392ca2d03a29757f79ff0fd9bc7c399d91054c50
alisaidmoha1 <alisaidmoha1@gmail.com>
2021-10-04 14:11:30 -0400

    first commit

---
+++ Budget.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Budget.WebMVC.Startup))]
+namespace Budget.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
